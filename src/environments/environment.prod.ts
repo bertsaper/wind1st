97539cb3739ff5d0cbea176b3ada2a870e40b8b4
Bert R. Saper <bsaper@alanet.org>
2022-07-09 10:27:27 -0500

    removing token

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  open_weather_address: "https://api.openweathermap.org/data/2.5/weather?",
+  lat_default: "41.8757",
+  lon_default: "-87.6243",
+  open_weather_key: "&appid=679ebf5bf3ae4a3c2a84a403cc46986e"
+};
