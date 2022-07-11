07662f890eb74fa4f42177b41421bf819672a113
Bert R. Saper <bsaper@alanet.org>
2022-07-10 19:04:13 -0500

    now receiving lat and lng plus calliing only what we need

--- src/app/explore-container/explore-container.component.ts
+++ src/app/explore-container/explore-container.component.ts
@@ -20,61 +20,60 @@ const googleMapsKey = environment.google_maps_api_key
   styleUrls: ['./explore-container.component.scss'],
 })
 export class ExploreContainerComponent implements OnInit {
-  SearchPlacesForm: NgForm;
-  public shippingAddress: string;
+  SearchPlacesForm: NgForm
+  public shippingAddress: string
 
 
-  apiLoaded: Observable<boolean>;
+  apiLoaded: Observable<boolean>
 
-  constructor(httpClient: HttpClient, @Inject(DOCUMENT) private document: Document, private renderer2: Renderer2) {
-    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=' + googleMapsKey + '', 'callback')
-      .pipe(
-        map(() => true),
-        catchError(() => of(false)),
-      );
-  }
+  constructor(@Inject(DOCUMENT) private document: Document, private renderer2: Renderer2) {}
 
   ngOnInit() {
 
-    this.loadAutoComplete();
+    this.loadAutoComplete()
 
   }
 
   private loadAutoComplete() {
+    // const url = 'https://maps.googleapis.com/maps/api/js?key=' + googleMapsKey+ '&libraries=places&v=weekly';
     const url = 'https://maps.googleapis.com/maps/api/js?key=' + googleMapsKey+ '&libraries=places&v=weekly';
-    this.loadScript(url).then(() => this.initAutocomplete());
+
+    this.loadScript(url).then(() => this.initAutocomplete())
   }
   private loadScript(url) {
     return new Promise((resolve, reject) => {
-      const script = this.renderer2.createElement('script');
-      script.type = 'text/javascript';
-      script.src = url;
-      script.text = ``;
-      script.async = true;
-      script.defer = true;
-      script.onload = resolve;
-      script.onerror = reject;
-      this.renderer2.appendChild(this.document.head, script);
+      const script = this.renderer2.createElement('script')
+      script.type = 'text/javascript'
+      script.src = url
+      script.text = ``
+      script.async = true
+      script.defer = true
+      script.onload = resolve
+      script.onerror = reject
+      this.renderer2.appendChild(this.document.head, script)
     })
   }
   initAutocomplete() {
     const input = document.getElementById("txtSearchPlaces") as HTMLInputElement;
     const autocomplete = new google.maps.places.Autocomplete(input);
     autocomplete.setFields([
-      "address_components",
       "geometry",
-      "icon",
-      "name"
+      "name",
     ]);
+
+    
     autocomplete.addListener("place_changed", () => {
-      const place = autocomplete.getPlace();
-      if (!place.geometry) {
+      const place = autocomplete.getPlace()
+      
+      console.log(place.geometry.location.lat())
+      console.log(place.geometry.location.lng())
+      if (!place) {
         // User entered the name of a Place that was not suggested and
         // pressed the Enter key, or the Place Details request failed.
-        alert('No details available for input:' + input.value);
-        return;
+        alert('No details available for input:' + input.value)
+        return
       } else {
-        return;
+        return
       }
     });
   }
