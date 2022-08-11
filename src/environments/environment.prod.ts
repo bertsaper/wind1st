6a8b0951847e303c18d355e0eb9672c1b8d21a15
Bert Saper <bert.saper@gmail.com>
2022-08-10 19:29:37 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   open_weather_address: "https://api.openweathermap.org/data/2.5/weather?",
 
-  open_weather_key: "&appid=YOUR OPEN WEATHER KEYe",
+  open_weather_key: "&appid=YOUR OPEN WEATHER KEY",
   google_maps_api_key: "YOUR GOOGLE MAP KEY"
 };
