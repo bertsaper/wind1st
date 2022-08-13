85411445530cf6df471061aa0dc70d17713ea80e
Bert Saper <bert.saper@gmail.com>
2022-08-13 12:57:31 -0500

    Adding Generic Files

---
+++ environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  open_weather_address: "https://api.openweathermap.org/data/2.5/weather?",
+
+  open_weather_key: "&appid=YOUR_OPEN_WEATHER_KEY",
+  google_maps_api_key: "YOUR_GOOGLE_MAPS_KEY"
+};
