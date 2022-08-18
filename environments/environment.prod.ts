2627da98a359f55ff56f9029b114e4e73b62a20f
Bert Saper <bert.saper@gmail.com>
2022-08-17 21:17:24 -0500

    camel casing environment variables

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  open_weather_address: "https://api.openweathermap.org/data/2.5/weather?",
+  openWeatherAddress: `https://api.openweathermap.org/data/2.5/weather?`,
 
-  open_weather_key: "&appid=YOUR_OPEN_WEATHER_KEY",
-  google_maps_api_key: "YOUR_GOOGLE_MAPS_KEY"
+  openWeatherKey: `&appid=YOUR_OPEN_WEATHER_KEY`,
+  googleMapsApiKey: `YOUR_GOOGLE_MAPS_KEY`
 };
