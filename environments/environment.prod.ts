5bf8de5df6b8f8ab3d8604231a9e216809a872bd
Bert Saper <bert.saper@gmail.com>
2022-08-18 14:17:17 -0500

    Delete environments directory

--- environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  openWeatherAddress: `https://api.openweathermap.org/data/2.5/weather?`,
-
-  openWeatherKey: `&appid=YOUR_OPEN_WEATHER_KEY`,
-  googleMapsApiKey: `YOUR_GOOGLE_MAPS_KEY`
-};
