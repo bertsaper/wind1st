3b6219dc8be517609aa03c8503b34774ad394c0f
Bert Saper <bert.saper@gmail.com>
2022-08-31 19:46:08 -0500

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  openWeatherAddress: `https://api.openweathermap.org/data/2.5/weather?`,
-
-  openWeatherKey: `&appid=328d67cd770faee9a34e5a9222741be7`,
-  googleMapsApiKey: `AIzaSyAOqS8MqbB8QlZ8rP-FpGLctYJGx936E4s`
-};
