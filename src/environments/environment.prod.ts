fbb0ecb7c1bb877f14f7f5ee72553669b92d3114
Bert <Saper>
2022-09-03 19:20:49 -0500

    aading linebrask

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   openWeatherAddress: `https://api.openweathermap.org/data/2.5/weather?`,
 
-  openWeatherKey: `&appid=c75417b460380722fe8fc0ff6201e98a`,
-  googleMapsApiKey: `AIzaSyAOqS8MqbB8QlZ8rP-FpGLctYJGx936E4s`
+  openWeatherKey: `&appid=Your_OpenWeather_API_Key`,
+  googleMapsApiKey: `Your_Google_Places_API_Key`
 };
+
