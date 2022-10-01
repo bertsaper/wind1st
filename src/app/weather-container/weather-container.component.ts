fcca5133b802ef3c441aed98d55cd505162134ec
Bert <Saper>
2022-10-01 17:55:45 -0500

    adjusting logic on location update

--- src/app/weather-container/weather-container.component.ts
+++ src/app/weather-container/weather-container.component.ts
@@ -66,7 +66,7 @@ export class ExploreContainerComponent implements OnInit {
 
   useDeviceIsSet: boolean
 
-  locationUnavailable: boolean
+  locationUnavailable = false
 
   /*
   * Needed for Imperial / Metric Selection
@@ -102,12 +102,12 @@ export class ExploreContainerComponent implements OnInit {
     private http: HttpClient,
     public router: Router,
     private renderer: Renderer2,
-  ) {
-    this.getLocation()
-  }
+  ) { }
 
 
   ngOnInit() {
+
+    this.getLocation()
 
     if (localStorage.getItem(this.weatherLocationStorage) === null) {
       this.router.navigate([this.locationSettings])
@@ -258,6 +258,10 @@ export class ExploreContainerComponent implements OnInit {
     if (this.updateLocal) {
       clearInterval(this.updateLocal)
     }
+
+    /*
+    * Shorthand not used below because some devices did not repond
+    */
 
     if (this.useDeviceIsSet && !this.locationUnavailable) {
 
