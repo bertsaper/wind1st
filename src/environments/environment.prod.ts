201338db0eed158aad23b844ea83d954a1a7ebc0
Bert <bert.saper@gmail.com>
2025-08-24 11:27:54 -0500

    Your commit message here

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  openWeatherAddress: `https://api.openweathermap.org/data/2.5/weather?`,
-
-  openWeatherKey: `&appid=Your_OpenWeather_API_Key`,
-  googleMapsApiKey: `Your_Google_Places_API_Key_A`
-};
